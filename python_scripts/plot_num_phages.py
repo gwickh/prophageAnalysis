32ff1763ffa8cf236fab19644f7cd0d584fa0fb0
Gregory Wickham <gregory.wickham@quadram.ac.uk>
2024-03-19 22:59:17 +0000

    plot as swarm instead of bar

--- python_scripts/plot_num_phages.py
+++ python_scripts/plot_num_phages.py
@@ -3,50 +3,76 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-csv_path = "~/whitchurch_group/PRO_Foodborne_Pseudomonas_Prophages/prophage_regions/"
-phage_predictions = pd.read_csv(csv_path+"concatenated_predictions_summary.csv")
+project_path = "~/whitchurch_group/PRO_Foodborne_Pseudomonas_Prophages/prophage_regions/"
+phage_predictions = pd.read_csv(project_path+"concatenated_predictions_summary.csv")
 
 phage_predictions['genome'] = phage_predictions['genome'].str.replace(r'_contigs', '')
 
-mean_phage_predictions = phage_predictions\
+#plot counts of predictions as points
+count_phage_predictions = phage_predictions\
     .groupby(["genome", "prediction_tool"])\
     .count()\
-    .groupby("genome")\
-    .mean()\
     .sort_values(by = "contig", ascending = False)\
-    .rename(columns = {"contig": "mean_phage_predictions"})\
+    .rename(columns = {"contig": "count_phage_predictions"})\
     .drop(columns = ["prophage_start", "prophage_end", "length"])\
     .reset_index()
+    
+count_phage_predictions_mean = count_phage_predictions\
+    .groupby("genome")\
+    .mean("count_phage_predictions")\
+    .rename(columns = {"count_phage_predictions": "mean_count"})\
 
-phage_predictions = phage_predictions.merge(mean_phage_predictions, on="genome")
+count_phage_predictions = count_phage_predictions.merge(count_phage_predictions_mean, on="genome")
 
-#plot counts of predictions as bars
 plt.figure(figsize=(14,6))
-g = sns.countplot(
-    data = phage_predictions,
-    x = "genome",
-    hue = "prediction_tool",
-    order=phage_predictions.sort_values(
-        'mean_phage_predictions', 
-        ascending = False
-    ).genome
-)
-g.set_xticklabels(g.get_xticklabels(), rotation=45, horizontalalignment='right')
-g.tick_params(labelsize=10)
-g.set_title('Number of Predicted Phage Regions', fontsize = 10)
-g.set_ylabel('Cumulative Count', fontsize = 10)
-g.set_xlabel('Genome', fontsize = 10)
-g.legend(fontsize='10', title_fontsize='10')
 plt.rcParams['figure.dpi'] = 600
 plt.rcParams['savefig.dpi'] = 600
-
-#plot counts of predictions as points
-g = sns.countplot(
-    data = phage_predictions,
+g2 = sns.swarmplot(
+    data = count_phage_predictions,
     x = "genome",
+    y = "count_phage_predictions",
     hue = "prediction_tool",
-    order=phage_predictions.sort_values(
-        'mean_phage_predictions', 
+    order=count_phage_predictions.sort_values(
+        'mean_count', 
         ascending = False
-    ).genome
-)
+        ).genome,
+    # linestyle = "",
+    # marker = "."
+    )
+xlim = g2.get_xlim()
+ylim = g2.get_ylim()
+sns.scatterplot(
+    data = count_phage_predictions,
+    ax = g2,
+    x = "genome", 
+    y = "mean_count", 
+    marker = 'x', 
+    color = 'black', 
+    s = 50,
+    zorder = 3, 
+    legend = False
+    )
+g2.set_xlim(xlim)
+g2.set_ylim(ylim)
+g2.set_xticklabels(
+    g2.get_xticklabels(), 
+    rotation=45, 
+    horizontalalignment='right'
+    )
+g2.tick_params(labelsize=10)
+g2.set_title(
+    'Number of Predicted Phage Regions', 
+    fontsize = 10
+    )
+g2.set_ylabel(
+    'Number of Predicted Prophage Regions >1000 bp', 
+    fontsize = 10
+    )
+g2.set_xlabel(
+    'Genome', 
+    fontsize = 10
+    )
+g2.legend(
+    fontsize='10', 
+    title_fontsize='10'
+    )
