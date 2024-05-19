4d59d3f0d24c2b8ce4d37356056bd3068df9c442
Gregory Wickham <gregory.wickham@quadram.ac.uk>
2024-05-19 17:29:52 +0000

    get query seq lengths

--- shell_scripts/prophage_blast.sh
+++ shell_scripts/prophage_blast.sh
@@ -18,15 +18,27 @@ fi
 
 conda activate blast
 
-makeblastdb -in PAO1.fa -out ./index/pao1 -dbtype 'nucl' -hash_index
-
 for k in *.fna
+    do 
+    makeblastdb \
+        -in $k \
+        -out $(basename $k .fna) \
+        -dbtype nucl \
+        -hash_index
+    done
+
+for k in *.fa 
     do 
     blastn \
         -query $k \
         -task blastn \
-        -db pao1 \
-        -outfmt "10 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore gaps" > ./$k.pao1_align.csv \
-        -evalue 1e-200; 
-    echo $k; 
+        -db index/$(basename $k .fa) \
+        -outfmt "10 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore gaps" > $(basename $k .fa)_align.csv \
+        -evalue 1e-200
+    done
+
+for k in *.fna
+    do 
+    awk '/^>/{if (l!="") print l; print; l=0; next}{l+=length($0)}END{print l}' $k | \
+        paste - - > $(basename $k .fna)_count.tsv
     done
